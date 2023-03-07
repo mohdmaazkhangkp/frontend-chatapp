ebf500c106b10d2abd9eb56a5f84bdb25fa03d9a
Maaz Khan <mohdmaazkhangkp@gmail.com>
2023-03-07 12:41:16 +0530

    proxy change

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/api",
+    "/",
     createProxyMiddleware({
       target: "https://chatappbackend-qp5h.onrender.com",
       changeOrigin: true,
